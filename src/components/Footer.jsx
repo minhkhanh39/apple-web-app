58f5f51622335fd90612682c380488b5f558e384
chuatevippro <nnguyenminhkhanh39@gmail.com>
2025-11-22 00:50:42 +0700

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { footerLinks } from "../constants/index.js";
 
 const Footer = () => {
     return (
@@ -6,12 +7,14 @@ const Footer = () => {
                 <p>More ways to shop: Find an Apple Store or other retailer near you. Or call 000800 040 1966.</p>
                 <img src="/logo.svg" alt="Apple logo" />
             </div>
+
             <hr />
 
             <div className="links">
                 <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+
                 <ul>
-                    {footerLinks.map((label, link) => (
+                    {footerLinks.map(({ label, link }) => (
                         <li key={label}>
                             <a href={link}>{label}</a>
                         </li>
@@ -21,5 +24,4 @@ const Footer = () => {
         </footer>
     )
 }
-
 export default Footer
