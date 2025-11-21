87098e1c68cf679c033881f2f7654cb286d0b5d4
chuatevippro <nnguyenminhkhanh39@gmail.com>
2025-11-22 00:22:17 +0700

    Implement Features Section

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 import Showcase from "./components/Showcase";
 import Performance from "./components/Performance";
-import Features from "./Features";
+import Features from "./components/Features";
 import Highlights from "./components/Highlights";
 import Footer from "./components/Footer";
 
