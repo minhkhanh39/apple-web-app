4ecf4d1429debc1bb08dd4d301184a763155659b
chuatevippro <nnguyenminhkhanh39@gmail.com>
2025-11-21 19:38:47 +0700

    'Fixed

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -26,7 +26,7 @@ const Showcase = () => {
     return (
         <section id="showcase">
             <div className="media">
-                <video src="/public/videos/game.mp4" loop muted autoPlay playsInline />
+                <video src="/videos/game.mp4" loop muted autoPlay playsInline />
                 <div className="mask">
                     <img src="/mask-logo.svg" />
                 </div>
